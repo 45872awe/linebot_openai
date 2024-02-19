f2e30182284494cf325c9ba0dc6f11cc47bc9d3a
45872awe <abc8305012005@gmail.com>
2024-02-19 19:17:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import Flask, request, abort
-import tempfile
 import os
+import sys
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
 from linebot.v3 import (
-    WebhookHandler
+     WebhookHandler
 )
 from linebot.v3.exceptions import (
     InvalidSignatureError
 )
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+)
 from linebot.v3.messaging import (
-    MessagingApiBlob,
     Configuration,
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
     TextMessage
 )
-from linebot.v3.webhooks import (
-    MessageEvent,
-    FileMessageContent,
-    TextMessageContent
-)
+
 import time
 import threading 
 import requests
@@ -44,43 +45,8 @@ handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 @app.route("/heroku_wake_up")
 def heroku_wake_up():
     return "Hey!Wake Up!!"
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
 
 
-import os
-import sys
-from argparse import ArgumentParser
-
-from flask import Flask, request, abort
-from linebot import (
-    WebhookParser
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
 
 app = Flask(__name__)
 
@@ -94,7 +60,7 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
-parser = WebhookParser(channel_secret)
+handler = WebhookHandler(channel_secret)
 
 configuration = Configuration(
     access_token=channel_access_token
@@ -103,50 +69,40 @@ configuration = Configuration(
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # parse webhook body
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessageContent):
-            continue
-        with ApiClient(configuration) as api_client:
-            line_bot_api = MessagingApi(api_client)
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=event.message.text)]
-                )
-            )
-
     return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def message_text(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
     app.run(debug=options.debug, port=options.port)
-
-
-
-@handler.default()
-def default(event):
-    print(event)
-if __name__ == "__main__":
-    app.run()
